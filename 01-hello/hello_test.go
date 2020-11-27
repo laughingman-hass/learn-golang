83ab4ecca1a51f13f7ee684b003ff020e4890ddb
Hassan Khalid <hassan@razgriz.io>
2020-11-27 00:49:47 +0000

    name for Hello method now accepts empty string
    Now returns world if provided with an empty string

--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello("Hassan")
-    want := "Hello, Hassan"
-
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
+    assertCorrectMessage := func(t *testing.T, got, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
     }
+    
+    t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Hass")
+        want := "Hello, Hass"
+
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("say 'Hello, World' when an emptry string is supplicated", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, world"
+
+        assertCorrectMessage(t, got, want)
+    })
 }
