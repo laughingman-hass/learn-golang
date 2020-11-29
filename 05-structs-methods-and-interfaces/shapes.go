b43b4c2d01033a5161dc621ad9d94a7000c9f213
Hassan Khalid <hassan@razgriz.io>
2020-11-29 09:41:59 +0000

    Triangle added as a Shape with Area test

--- 05-structs-methods-and-interfaces/shapes.go
+++ 05-structs-methods-and-interfaces/shapes.go
@@ -31,3 +31,16 @@ func (c Circle) Area() float64 {
 func (c Circle) Perimeter() float64 {
 	return 2 * math.Pi * c.Radius
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
+func (t Triangle) Perimeter() float64 {
+	return 0
+}
