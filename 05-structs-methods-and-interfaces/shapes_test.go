13a733ab9814b34a27dc7d445d9099a24fad8b01
Hassan Khalid <hassan@razgriz.io>
2020-11-29 09:58:30 +0000

    refactored tests to provide better output with parametised cases

--- 05-structs-methods-and-interfaces/shapes_test.go
+++ 05-structs-methods-and-interfaces/shapes_test.go
@@ -4,35 +4,42 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	perimeterTests := []struct {
-		shape Shape
-		want  float64
+		name         string
+		shape        Shape
+		hasPerimeter float64
 	}{
-		{shape: Rectangle{Width: 10.0, Height: 10.0}, want: 40.0},
-        {shape: Circle{Radius: 5.0}, want: 31.41592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 10.0}, hasPerimeter: 40.0},
+		{name: "Circle", shape: Circle{Radius: 5.0}, hasPerimeter: 31.41592653589793},
 	}
 
 	for _, tt := range perimeterTests {
-		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %g, want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			actual := tt.shape.Perimeter()
+			if actual != tt.hasPerimeter {
+				t.Errorf("%#v: actual %g, expected %g", tt.name, actual, tt.hasPerimeter)
+			}
+		})
 	}
 }
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
-		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g, want %g", got, tt.want)
-		}
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			actual := tt.shape.Area()
+			if actual != tt.hasArea {
+				t.Errorf("%#v: actual %g, expected %g", tt.shape, actual, tt.hasArea)
+			}
+		})
 	}
 }
