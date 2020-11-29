339b3e0c733321926fc71cb6ce9b6932813ac9c6
Hassan Khalid <hassan@razgriz.io>
2020-11-29 10:42:07 +0000

    additional test for Withdraw with insufficient funds

--- 06-points-and-errors/wallet_test.go
+++ 06-points-and-errors/wallet_test.go
@@ -23,4 +23,16 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		actualError := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if actualError == nil {
+			t.Errorf("wanted an error but didn't get one")
+		}
+	})
 }
