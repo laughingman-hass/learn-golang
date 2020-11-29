c2739be915a4904365f5fa666688a4526f9ca23a
Hassan Khalid <hassan@razgriz.io>
2020-11-29 09:45:47 +0000

    tests refactored to use named parameters

--- 05-structs-methods-and-interfaces/shapes_test.go
+++ 05-structs-methods-and-interfaces/shapes_test.go
@@ -7,8 +7,8 @@ func TestPerimeter(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 10.0}, 40.0},
-		{Circle{5.0}, 31.41592653589793},
+		{shape: Rectangle{Width: 10.0, Height: 10.0}, want: 40.0},
+        {shape: Circle{Radius: 5.0}, want: 31.41592653589793},
 	}
 
 	for _, tt := range perimeterTests {
@@ -24,9 +24,9 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10.0}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
