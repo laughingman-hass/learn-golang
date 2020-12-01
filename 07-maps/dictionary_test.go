501be8b13b999c77e093384a02e9350df8674a9d
Hassan Khalid <hassan@razgriz.io>
2020-12-01 17:58:13 +0000

    improved Update to return error if word doesn't exist

--- 07-maps/dictionary_test.go
+++ 07-maps/dictionary_test.go
@@ -50,9 +50,20 @@ func TestUpdate(t *testing.T) {
 		dictionary := Dictionary{word: definition}
 		newDefinition := "new definition"
 
-		dictionary.Update(word, newDefinition)
+		err := dictionary.Update(word, newDefinition)
 
+		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
 	})
 }
 
