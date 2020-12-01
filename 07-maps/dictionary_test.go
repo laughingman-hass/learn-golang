43cc55242ccc95807e8e0ad07ee5b45282af943f
Hassan Khalid <hassan@razgriz.io>
2020-12-01 08:58:04 +0000

    improved Search to return error if word not found in dictionary

--- 07-maps/dictionary_test.go
+++ 07-maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	actual := dictionary.Search("test")
-	expected := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		actual, _ := dictionary.Search("test")
+		expected := "this is just a test"
 
-	assertStrings(t, actual, expected)
+		assertStrings(t, actual, expected)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		expected := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), expected)
+	})
 }
 
 func assertStrings(t *testing.T, actualString, expectedString string) {
