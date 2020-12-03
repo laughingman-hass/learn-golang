8d943d8855a53b1d85ec95ce57a615e050d09ce4
Hassan Khalid <hassan@razgriz.io>
2020-12-03 19:37:59 +0000

    revised Greet function with dependency injection for testing

---
+++ 08-dependency-injection/greet_test.go
@@ -0,0 +1,18 @@
+package greet
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Hassan")
+
+	actual := buffer.String()
+	expected := "Hello, Hassan"
+
+	if actual != expected {
+		t.Errorf("actual %q, expected %q", actual, expected)
+	}
+}
