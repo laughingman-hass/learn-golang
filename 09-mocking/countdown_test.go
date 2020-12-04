04beae103ee4384de7a39b0cf3f93f22b3a95197
Hassan Khalid <hassan@razgriz.io>
2020-12-04 15:07:43 +0000

    removed the sleep spy as the Countdown spy can be used

--- 09-mocking/countdown_test.go
+++ 09-mocking/countdown_test.go
@@ -2,31 +2,57 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("counts down with sleep", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, &CountdownOperationsSpy{})
 
-	actual := buffer.String()
-	expected := "3\n2\n1\nGo!"
+		actual := buffer.String()
+		expected := "3\n2\n1\nGo!"
 
-	if actual != expected {
-		t.Errorf("actual %q, expected %q", actual, expected)
-	}
+		if actual != expected {
+			t.Errorf("actual %q, expected %q", actual, expected)
+		}
+	})
 
-    if spySleeper.Calls != 4 {
-        t.Errorf("not enough calls to sleeper, expected 4, actual %d", spySleeper.Calls)
-    }
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expected := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(expected, spySleepPrinter.Calls) {
+			t.Errorf("expected calls %v got %v", expected, spySleepPrinter.Calls)
+		}
+	})
 }
 
-type SpySleeper struct {
-	Calls int
+const write = "write"
+const sleep = "sleep"
+
+type CountdownOperationsSpy struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
