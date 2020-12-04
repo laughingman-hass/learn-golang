0d1e072f948191d508fc93ef82666b432c72e08f
Hassan Khalid <hassan@razgriz.io>
2020-12-04 14:14:40 +0000

    call to sleep mocked out so it can be tested without acutaly sleeping

--- 09-mocking/countdown_test.go
+++ 09-mocking/countdown_test.go
@@ -7,8 +7,9 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	actual := buffer.String()
 	expected := "3\n2\n1\nGo!"
@@ -16,4 +17,16 @@ func TestCountdown(t *testing.T) {
 	if actual != expected {
 		t.Errorf("actual %q, expected %q", actual, expected)
 	}
+
+    if spySleeper.Calls != 4 {
+        t.Errorf("not enough calls to sleeper, expected 4, actual %d", spySleeper.Calls)
+    }
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
 }
