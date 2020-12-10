4a00ba57a39a371650f5a62177c67dc876e76f10
Hassan Khalid <hassan@razgriz.io>
2020-12-10 04:38:00 +0000

    used context to cancel request if requested to cancel

--- 14-context/context_server_test.go
+++ 14-context/context_server_test.go
@@ -1,29 +1,64 @@
 package context_server
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestHandler(t *testing.T) {
-	data := "hello, world"
-	svr := Server(&StubStore{data})
+	t.Run("correctly serves a http GET request", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	respnse := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		respnse := httptest.NewRecorder()
 
-	svr.ServeHTTP(respnse, request)
+		svr.ServeHTTP(respnse, request)
 
-	if respnse.Body.String() != data {
-		t.Errorf(`actual "%s", expected "%s"`, respnse.Body.String(), data)
-	}
+		if respnse.Body.String() != data {
+			t.Errorf(`actual "%s", expected "%s"`, respnse.Body.String(), data)
+		}
+
+		if store.cancelled {
+			t.Errorf("it should not have cancelled the store")
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("store was not told to cancel")
+		}
+	})
 }
 
-type StubStore struct {
-	response string
+type SpyStore struct {
+	response  string
+	cancelled bool
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
