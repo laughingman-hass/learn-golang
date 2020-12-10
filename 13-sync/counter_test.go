a76975b2aa1df080367427f58db6264948d2eb05
Hassan Khalid <hassan@razgriz.io>
2020-12-10 04:04:50 +0000

    mutex added to counter for safe concurrency

--- 13-sync/counter_test.go
+++ 13-sync/counter_test.go
@@ -1,6 +1,9 @@
 package sync_counter
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -12,6 +15,24 @@ func TestCounter(t *testing.T) {
 		if counter.Value() != 3 {
 			t.Errorf("actual %d, expected %d", counter.Value(), 3)
 		}
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		expectedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(expectedCount)
+
+		for i := 0; i < expectedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, expectedCount)
 	})
 }
 
