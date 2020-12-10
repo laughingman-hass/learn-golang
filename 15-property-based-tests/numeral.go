ac61f1324100d1890a33367b0992e98c3abe7825
Hassan Khalid <hassan@razgriz.io>
2020-12-10 05:54:48 +0000

    refacted to keep working with string builder

--- 15-property-based-tests/numeral.go
+++ 15-property-based-tests/numeral.go
@@ -5,12 +5,13 @@ import "strings"
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	if arabic == 4 {
-		return "IV"
-	}
-
 	for i := 0; i < arabic; i++ {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
+
 	return result.String()
 }
