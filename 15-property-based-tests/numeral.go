2e8637a858806a33fdf66d89cdf004d5e413d3e9
Hassan Khalid <hassan@razgriz.io>
2020-12-10 05:55:48 +0000

    improved converter for 5

--- 15-property-based-tests/numeral.go
+++ 15-property-based-tests/numeral.go
@@ -6,6 +6,10 @@ func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
 	for i := 0; i < arabic; i++ {
+		if arabic == 5 {
+			result.WriteString("V")
+			break
+		}
 		if arabic == 4 {
 			result.WriteString("IV")
 			break
