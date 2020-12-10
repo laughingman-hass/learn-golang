2f658c4e45f72b8f4130a1cdb3667e11174f7e3d
Hassan Khalid <hassan@razgriz.io>
2020-12-10 05:39:22 +0000

    improved converter for 2

--- 15-property-based-tests/numeral_test.go
+++ 15-property-based-tests/numeral_test.go
@@ -3,10 +3,21 @@ package numeral
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	actual := ConvertToRoman(1)
-	expected := "I"
+	t.Run("1 gets converted t I", func(t *testing.T) {
+		actual := ConvertToRoman(1)
+		expected := "I"
 
-	if actual != expected {
-		t.Errorf("actual %q, expected %q", actual, expected)
-	}
+		if actual != expected {
+			t.Errorf("actual %q, expected %q", actual, expected)
+		}
+	})
+
+	t.Run("2 gets converted to II", func(t *testing.T) {
+		actual := ConvertToRoman(2)
+		expected := "II"
+
+		if actual != expected {
+			t.Errorf("actual %q, expected %q", actual, expected)
+		}
+	})
 }
