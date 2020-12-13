972bad4bc3df5be97d721a73f116896dd9640bd2
Hassan Khalid <hassan@razgriz.io>
2020-12-13 12:31:22 +0000

    new test case to provide score of a different player
    hardcoded play name check. no store

--- 17-application/server_test.go
+++ 17-application/server_test.go
@@ -20,4 +20,18 @@ func TestGETPlayers(t *testing.T) {
 			t.Errorf("actual %q, exected %q", actual, expected)
 		}
 	})
+
+	t.Run("returns LaughingMan's score", func(t *testing.T) {
+        request, _ := http.NewRequest(http.MethodGet, "/players/LaughingMan", nil)
+        response := httptest.NewRecorder()
+
+        PlayerServer(response, request)
+
+        actual := response.Body.String()
+        expected := "10"
+
+        if actual != expected {
+            t.Errorf("actual %q, expected %q", actual, expected)
+        }
+	})
 }
