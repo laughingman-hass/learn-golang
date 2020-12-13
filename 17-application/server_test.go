3f9f78e8383123d1260bd2fb1276664b06ff2432
Hassan Khalid <hassan@razgriz.io>
2020-12-13 14:42:26 +0000

    POST request for player scores responding with correct status code

--- 17-application/server_test.go
+++ 17-application/server_test.go
@@ -46,6 +46,22 @@ func TestGETPlayers(t *testing.T) {
 	})
 }
 
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Hass", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertRespnseStatus(t, response.Code, http.StatusAccepted)
+	})
+}
+
 func newGetScoreRequest(name string) *http.Request {
 	requst, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
 	return requst
