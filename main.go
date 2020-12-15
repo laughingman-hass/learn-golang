8fde0aeebbdcce4a56bdabb62c75be0c6d06469b
Hassan Khalid <hassan@razgriz.io>
2020-12-15 18:42:17 +0000

    home page turned into a template

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -29,10 +33,18 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
+
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+
 	http.ListenAndServe(":3000", r)
 }
