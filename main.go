420b5d411fa429cdecbb6ed56ee2e2b69beda1d5
Hassan Khalid <hassan@razgriz.io>
2020-12-15 19:01:47 +0000

    contact page turned into a template

--- main.go
+++ main.go
@@ -8,17 +8,23 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if err := homeTemplate.Execute(w, nil); err != nil {
+        panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:dev@razgriz\">dev@razgriz<a>.")
+    if err := contactTemplate.Execute(w, nil); err != nil {
+        panic(err)
+    }
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +41,11 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
