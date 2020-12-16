7653b146ad80e70957ee55cef8e92fb43befa7e9
Hassan Khalid <hassan@razgriz.io>
2020-12-16 13:39:43 +0000

    layout passed to each view and execute updated to use the layout

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.Execute(w, nil)
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
 
 	if err != nil {
 		panic(err)
@@ -25,7 +25,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.Execute(w, nil)
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
 
 	if err != nil {
 		panic(err)
@@ -34,7 +34,7 @@ func contact(w http.ResponseWriter, r *http.Request) {
 
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := faqView.Template.Execute(w, nil)
+	err := faqView.Template.ExecuteTemplate(w, faqView.Layout, nil)
 
 	if err != nil {
 		panic(err)
@@ -44,7 +44,7 @@ func faq(w http.ResponseWriter, r *http.Request) {
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	err := notFoundView.Template.Execute(w, nil)
+	err := notFoundView.Template.ExecuteTemplate(w, notFoundView.Layout, nil)
 
 	if err != nil {
 		panic(err)
@@ -52,10 +52,10 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
-	faqView = views.NewView("views/faq.gohtml")
-	notFoundView = views.NewView("views/not_found.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	notFoundView = views.NewView("bootstrap", "views/not_found.gohtml")
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
