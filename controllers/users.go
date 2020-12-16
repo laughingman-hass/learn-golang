7029891df20ab5b5735ebc28f43a5a12fdb2eafe
Hassan Khalid <hassan@razgriz.io>
2020-12-16 20:09:48 +0000

    created UsersController to hangle users new

---
+++ controllers/users.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"learn-golang/views"
+	"net/http"
+)
+
+func NewUsers() *UsersController {
+	return &UsersController{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type UsersController struct {
+	NewView *views.View
+}
+
+func (u UsersController) New(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
