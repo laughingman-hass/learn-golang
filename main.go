777c578e9ff515807f5c6711224c56fb202f48ce
Hassan Khalid <hassan@razgriz.io>
2020-12-17 16:25:04 +0000

    updated the router to use the Static actions for static pages

--- main.go
+++ main.go
@@ -2,53 +2,22 @@ package main
 
 import (
 	"learn-golang/controllers"
-	"learn-golang/views"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var (
-	homeView     *views.View
-	contactView  *views.View
-	faqView      *views.View
-	notFoundView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(faqView.Render(w, nil))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	must(notFoundView.Render(w, nil))
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
-	notFoundView = views.NewView("bootstrap", "views/not_found.gohtml")
+	staticController := controllers.NewStatic()
 	usersController := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.NotFoundHandler = http.Handler(staticController.NotFound)
 
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
-	r.HandleFunc("/faq", faq).Methods("GET")
+	r.Handle("/", staticController.Home).Methods("GET")
+	r.Handle("/contact", staticController.Contact).Methods("GET")
+	r.Handle("/faq", staticController.FAQ).Methods("GET")
+
 	r.HandleFunc("/signup", usersController.New).Methods("GET")
 	r.HandleFunc("/signup", usersController.Create).Methods("POST")
 
