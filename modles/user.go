047f08b0bf4d76acf1ed5a0a5121fe884971a7bb
Hassan Khalid <hassan@razgriz.io>
2020-12-18 23:24:01 +0000

    created UserService with a find by id function

--- modles/user.go
+++ modles/user.go
@@ -1,9 +1,33 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var (
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func (us *UserService) ByID(id int) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
 
 type User struct {
 	gorm.Model
