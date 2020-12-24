9ead10e9dc276bd80ace4bf4f165c1946b9f855b
Hassan Khalid <hassan@razgriz.io>
2020-12-24 12:06:53 +0000

    created require email validator function

--- models/user.go
+++ models/user.go
@@ -150,6 +150,7 @@ func (uv *userValidator) Create(user *User) error {
 		uv.defaultSessionToken,
 		uv.hmacSessionToken,
 		uv.normalizeEmail,
+		uv.requireEmail,
 	)
 	if err != nil {
 		return err
@@ -163,6 +164,7 @@ func (uv *userValidator) Update(user *User) error {
 		uv.bcryptPassword,
 		uv.hmacSessionToken,
 		uv.normalizeEmail,
+		uv.requireEmail,
 	)
 	if err != nil {
 		return err
@@ -236,6 +238,13 @@ func (uv *userValidator) idGreaterThan(n uint) userValFunc {
 func (uv *userValidator) normalizeEmail(user *User) error {
 	user.Email = strings.ToLower(user.Email)
 	user.Email = strings.TrimSpace(user.Email)
+	return nil
+}
+
+func (uv *userValidator) requireEmail(user *User) error {
+	if user.Email == "" {
+		return errors.New("Email address is required")
+	}
 	return nil
 }
 
