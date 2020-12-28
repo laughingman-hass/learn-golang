898671491be5720812b03fe2197421df479a2ae4
Hassan Khalid <hassan@razgriz.io>
2020-12-28 11:40:10 +0000

    created privateError for internal server error

--- models/errors.go
+++ models/errors.go
@@ -3,16 +3,17 @@ package models
 import "strings"
 
 var (
-	ErrNotFound             modelError = "models: resource not found"
-	ErrIDInvalid            modelError = "models: ID provided was invalid"
-	ErrEmailRequired        modelError = "models: email address is required"
-	ErrEmailInvalid         modelError = "models: email address is not valid"
-	ErrEmailNotUnique       modelError = "models: email address already in use"
-	ErrPasswordIncorrect    modelError = "models: incorrect password provided"
-	ErrPasswordTooShort     modelError = "models: password must be 8 characters long"
-	ErrPasswordRequired     modelError = "models: password is required"
-	ErrSessionTokenInvalid  modelError = "models: session token must be at least 32 bytes"
-	ErrSessionTokenRequired modelError = "models: session token is required"
+	ErrNotFound          modelError = "models: resource not found"
+	ErrEmailRequired     modelError = "models: email address is required"
+	ErrEmailInvalid      modelError = "models: email address is not valid"
+	ErrEmailNotUnique    modelError = "models: email address already in use"
+	ErrPasswordIncorrect modelError = "models: incorrect password provided"
+	ErrPasswordTooShort  modelError = "models: password must be 8 characters long"
+	ErrPasswordRequired  modelError = "models: password is required"
+
+	ErrIDInvalid            privateError = "models: ID provided was invalid"
+	ErrSessionTokenInvalid  privateError = "models: session token must be at least 32 bytes"
+	ErrSessionTokenRequired privateError = "models: session token is required"
 )
 
 type modelError string
@@ -24,4 +25,10 @@ func (e modelError) Error() string {
 func (e modelError) Public() string {
 	s := strings.Replace(string(e), "models: ", "", 1)
 	return strings.Title(s)
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
