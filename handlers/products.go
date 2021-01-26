cbaefb3f21f460eda4efc54deb2ac9897ca73af8
Hassan Khalid <hassan@razgriz.io>
2021-01-26 17:50:04 +0000

    handler updated to use the Products type

--- handlers/products.go
+++ handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"learn-golang/intro-to-microservices/product-api/data"
 	"log"
 	"net/http"
@@ -17,10 +16,8 @@ type Products struct {
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
-
-    rw.Write(d)
 }
