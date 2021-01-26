f4c54cb79b6557084bdd378036fba605ba43e902
Hassan Khalid <hassan@razgriz.io>
2021-01-26 17:57:41 +0000

    extracted the GET request to separate function

--- handlers/products.go
+++ handlers/products.go
@@ -15,6 +15,16 @@ type Products struct {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
