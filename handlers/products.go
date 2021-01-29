5529246f963e2767a12ef342b3e2c37eb0c38d53
Hassan Khalid <hassan@razgriz.io>
2021-01-29 16:25:14 +0000

    middleware updated to check if data passed is valid

--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"learn-golang/intro-to-microservices/product-api/data"
 	"log"
 	"net/http"
@@ -74,7 +75,19 @@ func (p *Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 
 		err := prod.FromJSON(r.Body)
 		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
 			http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+			return
+		}
+
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+            http.Error(
+                rw,
+                fmt.Sprintf("Error validating product: %s", err),
+                http.StatusBadRequest,
+            )
 			return
 		}
 
