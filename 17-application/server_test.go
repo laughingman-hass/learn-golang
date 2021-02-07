8aca126237bbc449bb179c587ef8766b1aa604e8
Hassan Khalid <hassan@razgriz.io>
2021-02-07 07:32:06 +0000

    Integration test and InMemoryPlayerStore with RecordWin and get score methods
    server updated to 404 response for unknown player
    
    test and server for player POST request
    
    test refactored to spy on winCalls
    
    store interface updated to add RecordWin
    
    main func updated to add RecordWin to store
    
    server updated to recordwin for hardcoded player
    
    test and server updated for dynamic player
    
    refactored to DRY server
    
    Integration test and InMemoryPlayerStore with RecordWin and get score methods

--- 17-application/server_test.go
+++ 17-application/server_test.go
@@ -13,6 +13,7 @@ func TestGETPlayers(t *testing.T) {
 			"Pepper": 20,
 			"Floyd":  10,
 		},
+		nil,
 	}
 	server := &PlayerServer{&store}
 
@@ -46,6 +47,33 @@ func TestGETPlayers(t *testing.T) {
 	})
 }
 
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+		nil,
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it records wins on POST", func(t *testing.T) {
+		player := "Player"
+
+		request := newPostWinRequest(player)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+
+		if len(store.winCalls) != 1 {
+			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
+		}
+	})
+}
+
 func assertStatus(t testing.TB, got, want int) {
 	t.Helper()
 	if got != want {
@@ -66,11 +94,21 @@ func assertResponseBody(t testing.TB, got, want string) {
 	}
 }
 
+func newPostWinRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
 type StubPlayerStore struct {
-	scores map[string]int
+	scores   map[string]int
+	winCalls []string
 }
 
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
 	score := s.scores[name]
 	return score
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
 }
