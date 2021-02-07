516152dfd1d7ad8ab17e3c5459f6db0f6bef13c4
Hassan Khalid <hassan@razgriz.io>
2021-02-07 05:02:56 +0000

    created func to create a Client instance

--- facest/facest.go
+++ facest/facest.go
@@ -1,6 +1,23 @@
 package facest
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
+
+const (
+	BaseURLV1 = "https://api.facest.io/v1"
+)
+
+func NewClient(appKey string) *Client {
+	return &Client{
+		BaseURL: BaseURLV1,
+		apiKey:  appKey,
+		HTTPClient: &http.Client{
+			Timeout: time.Minute,
+		},
+	}
+}
 
 type Client struct {
 	BaseURL    string
