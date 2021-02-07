dbcb8235b59fcaf224c354cc6c36f76da5e72a43
Hassan Khalid <hassan@razgriz.io>
2021-02-07 05:31:51 +0000

    created the GetFaces function on client

--- facest/faces.go
+++ facest/faces.go
@@ -1,6 +1,13 @@
 package facest
 
-import "time"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 type FacesRes struct {
 	Code    int       `json:"code"`
@@ -24,4 +31,56 @@ type FaceImage struct {
 	ImageToken string    `json:"image_token"`
 	ImageURL   string    `json:"image_url"`
 	CreatedAt  time.Time `json:"created_at"`
+}
+
+type FacesListOptions struct {
+	Limit int `json:"limit"`
+	Page  int `json:"page"`
+}
+
+func (c *Client) GetFaces(ctx context.Context, options *FacesListOptions) (*FacesList, error) {
+	limit := 100
+	page := 1
+	if options != nil {
+		limit = options.Limit
+		page = options.Page
+	}
+
+	req, err := http.NewRequest(
+		http.MethodGet,
+		fmt.Sprintf("%s/faces?limit=%d&page=%d", c.BaseURL, limit, page),
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req = req.WithContext(ctx)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.apiKey))
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
+		var errRes errorResponse
+		if err := json.NewDecoder(res.Body).Decode(&errRes); err != nil {
+			return nil, fmt.Errorf("unknown error, status code: %d", res.StatusCode)
+		}
+
+		return nil, errors.New(errRes.Message)
+	}
+
+	var fullResponse FacesRes
+	if err := json.NewDecoder(res.Body).Decode(&fullResponse); err != nil {
+		return nil, err
+	}
+
+	return &fullResponse.Data, nil
 }
