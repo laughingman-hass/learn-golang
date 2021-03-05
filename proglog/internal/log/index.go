bb5ee0c7a8babed0bb3a1cb97841e0e863fedc65
Hassan Khalid <hassan@razgriz.io>
2021-03-05 02:03:46 +0000

    created Read function on index struct

--- proglog/internal/log/index.go
+++ proglog/internal/log/index.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"io"
 	"os"
 
 	"github.com/tysonmote/gommap"
@@ -46,6 +47,27 @@ type index struct {
 	file *os.File
 	mmap gommap.MMap
 	size uint64
+}
+
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+
+	if in == -1 {
+		out = uint32((i.size / entWidth) - 1)
+	} else {
+		out = uint32(in)
+	}
+
+	pos = uint64(out) * entWidth
+	if i.size < pos+entWidth {
+		return 0, 0, io.EOF
+	}
+
+	out = enc.Uint32(i.mmap[pos : pos+offWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
+	return out, pos, nil
 }
 
 func (i *index) Close() error {
