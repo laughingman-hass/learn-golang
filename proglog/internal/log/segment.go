cb47058fa6f419f6cf604a9aa69b18551adb3907
Hassan Khalid <hassan@razgriz.io>
2021-03-05 02:32:32 +0000

    created a segment struct with initialiser

---
+++ proglog/internal/log/segment.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     c,
+	}
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	s.store, err = newStore(storeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	s.index, err = newIndex(indexFile, c)
+	if err != nil {
+		return nil, err
+	}
+
+	off, _, err := s.index.Read(-1)
+	if err != nil {
+		s.nextOffset = baseOffset
+	} else {
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+
+	return s, nil
+}
+
+type segment struct {
+	store                  *store
+	index                  *index
+	baseOffset, nextOffset uint64
+	config                 Config
+}
