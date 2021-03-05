f174555f705429237fceffab3d329c7542511f85
Hassan Khalid <hassan@razgriz.io>
2021-03-05 04:45:28 +0000

    created a ProduceStream function on grpcServer

--- proglog/internal/server/server.go
+++ proglog/internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	api "proglog/api/v1"
 )
 
@@ -10,14 +11,44 @@ type Config struct {
 
 var _ api.LogServer = (*grpcServer)(nil)
 
-type grpcServer struct {
-	api.UnimplementedLogServer
-	*Config
-}
-
 func newgrpcServer(config *Config) (srv *grpcServer, err error) {
 	srv = &grpcServer{
 		Config: config,
 	}
 	return srv, nil
+}
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
+	*api.ProduceResponse,
+	error,
+) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+
+		err = stream.Send(res)
+		if err != nil {
+			return err
+		}
+	}
 }
