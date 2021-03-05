5ce8b3b9e7dfc63664898e058b041980cb79b4fd
Hassan Khalid <hassan@razgriz.io>
2021-03-05 01:56:01 +0000

    created a index struct

--- proglog/go.mod
+++ proglog/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/gorilla/mux v1.8.0
 	github.com/stretchr/testify v1.7.0
+	github.com/tysonmote/gommap v0.0.0-20201017170033-6edfc905bae0 // indirect
 	google.golang.org/protobuf v1.23.0
 )
---
+++ proglog/internal/log/index.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
+
+var (
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	entWidth        = offWidth + posWidth
+)
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	idx.size = uint64(fi.Size())
+	err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return idx, nil
+}
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
