417dff96b682689bff3b7d0f6813b097024c7569
Hassan Khalid <hassan@razgriz.io>
2021-03-05 03:16:53 +0000

    created setup function on Log struct

--- proglog/internal/log/log.go
+++ proglog/internal/log/log.go
@@ -1,6 +1,13 @@
 package log
 
-import "sync"
+import (
+	"io/ioutil"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
 
 func NewLog(dir string, c Config) (*Log, error) {
 	if c.Segment.MaxStoreBytes == 0 {
@@ -24,4 +31,45 @@ type Log struct {
 	Config        Config
 	activeSegment *segment
 	segments      []*segment
+}
+
+func (l *Log) setup() error {
+	files, err := ioutil.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+
+	var baseOffsets []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffsets = append(baseOffsets, off)
+	}
+
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+
+	for i := 0; i < len(baseOffsets); i++ {
+		err = l.newSegment(baseOffsets[i])
+		if err != nil {
+			return err
+		}
+		// baseOffsets contains dup for index and store so we skip the dup
+		i++
+	}
+
+	if l.segments == nil {
+		err = l.newSegment(
+			l.Config.Segment.InitialOffset,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
