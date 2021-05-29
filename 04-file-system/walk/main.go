37dccc63c9b75dc268828b1f2e9ea16bb51ab418
Hassan Khalid <hass.khalid@agilebits.com>
2021-05-29 10:27:24 +0100

    added logger to the delete action

--- 04-file-system/walk/main.go
+++ 04-file-system/walk/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -17,11 +18,14 @@ type config struct {
 	list bool
 	// delete files
 	del bool
+	// log destination writer
+	wLog io.Writer
 }
 
 func main() {
 	// Parsing command line flags
 	root := flag.String("root", ".", "Root directory to start")
+	logFile := flag.String("log", "", "Log deletes to this file")
 	// Action options
 	list := flag.Bool("list", false, "List files only")
 	del := flag.Bool("del", false, "Delete files")
@@ -31,11 +35,26 @@ func main() {
 
 	flag.Parse()
 
+	var (
+		f   = os.Stdout
+		err error
+	)
+
+	if *logFile != "" {
+		f, err = os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	c := config{
 		ext:  *ext,
 		size: *size,
 		list: *list,
 		del:  *del,
+		wLog: f,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -44,6 +63,8 @@ func main() {
 	}
 }
 func run(root string, out io.Writer, cfg config) error {
+	delLogger := log.New(cfg.wLog, "DELETED FILE: ", log.LstdFlags)
+
 	return filepath.Walk(
 		root,
 		func(path string, info os.FileInfo, err error) error {
@@ -62,7 +83,7 @@ func run(root string, out io.Writer, cfg config) error {
 
 			// Delete files
 			if cfg.del {
-				return delFile(path)
+				return delFile(path, delLogger)
 			}
 
 			// List is the default option if nothing else was set
