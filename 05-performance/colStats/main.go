86dea7877255013e9b05415e3ddef241663cc04c
Hassan Khalid <hass.khalid@agilebits.com>
2021-05-31 16:06:59 +0100

    changed currency model to workers with the limit to number CPUs

--- 05-performance/colStats/main.go
+++ 05-performance/colStats/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"sync"
 )
 
@@ -47,33 +48,45 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 	resCh := make(chan []float64)
 	errCh := make(chan error)
 	doneCh := make(chan struct{})
+	filesCh := make(chan string)
 
 	wg := sync.WaitGroup{}
 
+	// Loop through all files sending them through the channel
+	// so each one will be processed when a worker is available
+	go func() {
+		defer close(filesCh)
+		for _, fname := range filenames {
+			filesCh <- fname
+		}
+	}()
+
 	// Loop through all files and create a goroutine to process
 	// each one concurrently
-	for _, fname := range filenames {
+	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
 
-		go func(fname string) {
+		go func() {
 			defer wg.Done()
 
-			// Open the file for reading
-			f, err := os.Open(fname)
-			if err != nil {
-				errCh <- fmt.Errorf("Cannot open file: %s", err)
-				return
-			}
-			defer f.Close()
+			for fname := range filesCh {
+				// Open the file for reading
+				f, err := os.Open(fname)
+				if err != nil {
+					errCh <- fmt.Errorf("Cannot open file: %s", err)
+					return
+				}
+				defer f.Close()
 
-			// Parse the CSV into a slice of float64 numbers
-			data, err := csv2float(f, column)
-			if err != nil {
-				errCh <- err
-			}
+				// Parse the CSV into a slice of float64 numbers
+				data, err := csv2float(f, column)
+				if err != nil {
+					errCh <- err
+				}
 
-			resCh <- data
-		}(fname)
+				resCh <- data
+			}
+		}()
 	}
 
 	go func() {
